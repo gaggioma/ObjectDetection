7754d7655dc3e4d7e778380145e7efbcae59cfd1
Gaggio Marco <marco.gaggio@wind.it>
2020-09-22 12:51:20 +0200

    command

--- images/command.bat
+++ images/command.bat
@@ -2,22 +2,22 @@
 
 cd C:\Backup\Develop\Python\ObjectDetection\images
 echo Creating myImages...
-::python3 .\listCreator.py -r "C:\\Users\\magaggio\\Desktop\\MNIST-JPG-master\\MNIST-JPG-master\\output" -n train --type cutImages --numberofsamples 100
-::python3 .\listCreator.py -r "C:\\Users\\magaggio\\Desktop\\MNIST-JPG-master\\MNIST-JPG-master\\output" -n validation --type cutImages --numberofsamples 10
+::python3 .\listCreator.py -r "output" -n train --type cutImages --numberofsamples 100
+::python3 .\listCreator.py -r "output" -n validation --type cutImages --numberofsamples 10
 echo Create myImages succesfully!!
 
 echo Resizing image...
-python3 .\imageResize.py -r "C:\\Users\\magaggio\\Desktop\\EmptyDataset" -n train -s 620
-python3 .\imageResize.py -r "C:\\Users\\magaggio\\Desktop\\EmptyDataset" -n validation -s 620
+python3 .\imageResize.py -r "EmptyDataset" -n train -s 620
+python3 .\imageResize.py -r "EmptyDataset" -n validation -s 620
 echo Resizing succesfully!!
 
 echo Listing file...
-python3 .\listCreator.py -r "C:\\Users\\magaggio\\Desktop\\EmptyDataset" -n train --type list
-python3 .\listCreator.py -r "C:\\Users\\magaggio\\Desktop\\EmptyDataset" -n validation --type list
+python3 .\listCreator.py -r "EmptyDataset" -n train --type list
+python3 .\listCreator.py -r "EmptyDataset" -n validation --type list
 echo Listing succesfully!!
 
 echo Creating cfg...
-python3 .\listCreator.py -r "C:\\Users\\magaggio\\Desktop\\EmptyDataset" -n train --type cfg
+python3 .\listCreator.py -r "EmptyDataset" -n train --type cfg
 echo Create cfg succesfully!!
 
 pause
